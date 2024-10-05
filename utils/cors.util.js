d5eadaf66afd8017f589a64e6c373b9f2f1a33c9
satyamx <satyamsingh748846@gmail.com>
2024-10-05 20:28:43 +0530

    feat: correct cors origin

--- utils/cors.util.js
+++ utils/cors.util.js
@@ -2,7 +2,18 @@ import { config } from "../config/env.config.js";
 
 const allowedOrigins = config.ALLOWED_ORIGINS.split(",");
 export const corsOptions = {
-  origin: allowedOrigins,
+  origin: (origin, callback) => {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true); 
+    } else {
+      callback(new Error("Not allowed by CORS")); 
+    }
+  },
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-  credentials: true,
+  credentials: true, 
+  optionsSuccessStatus: 200, 
 };
